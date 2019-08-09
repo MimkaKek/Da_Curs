4b0f229ee9d462402c0ccf5569b7f7eeb579878b
Lisoleg555 <lisoleg555@yandex.ru>
2019-08-09 19:56:05 +0300

    main_help отвечает последнему варианту. Архиватор точти готов

--- Src/gen.cpp
+++ Src/gen.cpp
@@ -10,7 +10,7 @@ int main() {
 	unsigned long long int count;
 	std::cin >> count;
 	int ran;
-	/*
+	/* возможно я переборщил с рандомизатором
 	for (unsigned long long int i = 0; i < count; i++) {
 		ran = mersenne() % 128;
 		std::cout << (char) ((ran > 32 && ran != 127) ? ran : ((ran + 40)) % 127);
