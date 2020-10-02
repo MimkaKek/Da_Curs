349fa8ddef22ed63d951a8cfdc1cfb3490ef69a5
Pasalc <43461852+Pasalc@users.noreply.github.com>
2020-10-02 13:51:15 +0300

    LZ77
    some tweaks may be required

--- Src/LZ77.h
+++ Src/LZ77.h
@@ -1,49 +1,91 @@
 /* LZ77.h */
 #pragma once
 #include "Compressor.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <bitset>
+#include "BFile.h"
+
+class Compressor;
+
+
+// template <const int compressFloor = 2, const bool GREEDY = false, const int MAXCOMPARES = 75, const int CHARBITS = 8 , const int MATCHBITS = 4, const int DICTBITS = 13, const int HASHBITS = 10,
+//  const int SECTORBITS = 10>
 class LZ77: public Compressor
 {
+
 public:
+
+	typedef struct InitStruct{
+		const int compressFloor 		= 2;
+		const int comparesCeil 			= 75;
+		const int CHARBITS				= 8;
+		const int MATCHBITS 			= 4; 
+		const int DICTBITS 				= 13;
+		const int HASHBITS 				= 10;
+		const int SECTORBITS 			= 10;
+		const unsigned int MAXMATCH 	= (1 << MATCHBITS) + compressFloor - 1;
+		const unsigned int DICTSIZE		= 1 << DICTBITS;
+		const unsigned int HASHSIZE 	= 1 << HASHBITS;
+		const unsigned int SHIFTBITS	= (HASHBITS + compressFloor) / (compressFloor + 1);
+		const unsigned int SECTORLEN	= 1 << SECTORBITS;
+		const unsigned int SECTORAND	= (0xFFFF << SECTORBITS) & 0xFFFF;
+	} IStruct;
 	LZ77();
-	/* 
-	 * Что делает: Сжимает данные
-	 */
-	void Compress() override;
-
-	/* 
-	 * Что делает: Расжимает данные
-	 */
-	void Decompress() override;
-
-	/* 
-	 * Что делает: Инициализирует поисковую структуру(словарь) необходимую для сжатия
-	 */
+	LZ77(IStruct s){
+		dict=(unsigned char*)calloc(DICTSIZE + MAXMATCH, sizeof(char));
+		hash=(unsigned int*)calloc(HASHSIZE, sizeof(unsigned int));
+		nextlink=(unsigned int*)calloc(DICTSIZE, sizeof(unsigned int));
+	};
 	void InitEncode();
 
-	/* 
-	 * Что делает: Загружает словарь из потока ввода, начиная с позиции dictpos
-	 */
+	void Compress() override;
+	void Decompress() override;
+
 	unsigned int LoadDict(unsigned int dictpos);
-
-	/*
-	 * Что делает: Удаляет из словаря сектор dectpos
-	 */
 	void DeleteData(unsigned int dictpos);
-
-	/*
-	 * Что делает: Хэширует данные
-	 */
 	void HashData(unsigned int dictpos, unsigned int bytestodo);
-
-	/*
-	 * Что делает: Ищет в словаре совпадение со строкой начиная с позиции dictpos
-	 */
 	void FindMatch(unsigned int dictpos, unsigned int startlen);
-
-	/*
-	 * Что делает: Ищет в словаре совпадение символов для текущего сектора
-	 */
 	void DictSearch(unsigned int dictpos, unsigned int bytestodo);
 
-	virtual ~LZ77();
+	void SendChar(unsigned int character);
+	void SendMatch(unsigned int matchlen, unsigned int matchdistance);
+	
+	unsigned int ReadBits(unsigned int numbits);
+	void SendBits(unsigned int bits, unsigned int numbits);
+	virtual ~LZ77(){};
+//private:
+	const int compressFloor 		= 2;
+	const int comparesCeil 			= 75;
+	const int CHARBITS				= 8;
+	const int MATCHBITS 			= 4; 
+	const int DICTBITS 				= 13;
+	const int HASHBITS 				= 10;
+	const int SECTORBITS 			= 10;
+	const unsigned int MAXMATCH 	= (1 << MATCHBITS) + compressFloor - 1;
+	const unsigned int DICTSIZE		= 1 << DICTBITS;
+	const unsigned int HASHSIZE 	= 1 << HASHBITS;
+	const unsigned int SHIFTBITS	= (HASHBITS + compressFloor) / (compressFloor + 1);
+	const unsigned int SECTORLEN	= 1 << SECTORBITS;
+	const unsigned int SECTORAND	= (0xFFFF << SECTORBITS) & 0xFFFF;
+	/* dictionary plus MAXMATCH extra chars for string comparisions */
+	unsigned char* dict;
+	/* hashtable & link list table */
+	unsigned int *hash, *nextlink;
+	/* misc. global variables */
+	unsigned int
+		counter=0,
+		matchlength=0,
+		matchpos=0,
+		bitbuf=0,
+		bitsin=0,
+		masks[17] = { 0,1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383,32767,65535 };
+
+		FILE *infile, *outfile;
 };
